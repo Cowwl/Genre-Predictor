6f207fa1fa9ceb82cb76457cc824f81f4ed63c7b
Cowwl <84736016+Cowwl@users.noreply.github.com>
2022-09-13 00:36:29 +0530

    basic frontend created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,49 @@
 import './App.css';
 import './components/Navbar'
 import Navbar from './components/Navbar';
-import {Div} from 'atomize';
+import { Div, Text, Button, Icon } from 'atomize';
+import Wave from 'react-wavify'
 
 function App() {
   return (
-    <Div bg="#12ADC1" h="100vh">
-      <Navbar/>
+    <Div d="flex" flexDir="column" bg="#191A19" justify="space-between" align="center" h="100vh">
+      <Navbar />
+      <Div p="2%">
+        <Text textSize="display2" textWeight="300" textColor="#D8E9A8" fontFamily="Questrial">
+          Upload your music and know it's genre among the 8 major genres.
+        </Text>
+      </Div>
+      <Button
+        prefix={
+          <Icon
+            name="Upload"
+            size="45px"
+            color="#191A19"
+            m={{ r: "1rem" }}
+          />
+        }
+        h="10%"
+        w="20%"
+        bg="#4E9F3D"
+        fontFamily="Questrial"
+        textWeight="700"
+        textColor="#191A19"
+        rounded="circle"
+        p={{ r: "1.5rem", l: "1rem" }}
+        hoverShadow="4"
+        textSize="display1"
+      >
+        Upload
+      </Button>
+      <Wave fill='#4E9F3D'
+        paused={false}
+        options={{
+          height: 40,
+          amplitude: 30,
+          speed: 0.4,
+          points: 3
+        }}
+      />
     </Div>
   );
 }
--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import "./Navbar.css"
 
 const Navbar = () => {
     return (
-        <Div d="flex" bg="#0295A9" w="100%" h="auto" justify="center" hoverShadow="3" className="nav" p="1%">
-            <Text textColor="#FDD037" textSize="display3" textWeight="600" fontFamily="Questrial">
+        <Div d="flex" bg="#1E5128" w="100%" h="auto" justify="center" hoverShadow="3" className="nav" p="1%">
+            <Text textColor="#D8E9A8" textSize="display3" textWeight="900" fontFamily="Questrial">
                 Genre Predictor
             </Text>
         </Div>
