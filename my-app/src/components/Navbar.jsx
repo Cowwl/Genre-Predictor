09af37b085ae72937310b3f0412d9a0f6247b86f
Cowwl <84736016+Cowwl@users.noreply.github.com>
2022-09-13 01:23:47 +0530

    backend struct created

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css"
 
 const Navbar = () => {
     return (
-        <Div d="flex" bg="#1E5128" w="100%" h="auto" justify="center" hoverShadow="3" className="nav" p="1%">
+        <Div d="flex" bg="#3b6a1c" w="100%" h="auto" justify="center" hoverShadow="3" className="nav" p="1%">
             <Text textColor="#D8E9A8" textSize="display3" textWeight="900" fontFamily="Questrial">
                 Genre Predictor
             </Text>
